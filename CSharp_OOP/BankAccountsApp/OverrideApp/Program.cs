dbb8474cf3197e3e1179625d469772c20e34d932
Md Rokonuzzaman Reza <44805545+EmonRezaBD@users.noreply.github.com>
2025-02-03 14:18:41 +0600

    Override keyword

---
+++ CSharp_OOP/BankAccountsApp/OverrideApp/Program.cs
@@ -0,0 +1,31 @@
+﻿public class student
+{
+    public string name { get; set; }
+    public int age { get; set; }
+    public double averageGrade { get; set; }
+
+    public override string ToString()
+    {
+        return name +" "+age+" "+"("+averageGrade+")";
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num = 5;
+        student s = new student();
+        s.name = "Sladina";
+        s.age = 23;
+        s.averageGrade = 10;
+
+
+        Console.WriteLine("Your num: " + num.ToString());
+        Console.WriteLine(s.name);
+
+        Console.WriteLine(s.ToString());
+        Console.WriteLine(s);
+    }
+
+}
