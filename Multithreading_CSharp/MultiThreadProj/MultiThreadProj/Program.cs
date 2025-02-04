74d99f23bc58a774dde35a3f1490634561263708
Md Rokonuzzaman Reza <44805545+EmonRezaBD@users.noreply.github.com>
2025-02-04 12:07:27 +0600

    Three threads

---
+++ Multithreading_CSharp/MultiThreadProj/MultiThreadProj/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+namespace MultiThreadingProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Welcome to Dotnet world!");
+            //Console.ReadKey();
+            //Press : Debug -> Windows -> Thread
+            //Thread t = Thread.CurrentThread;
+            //t.Name = "Main Thread";
+            //Console.WriteLine("Current Thread Name: "+t.Name);
+           /* Method1();
+            Method2();
+            Method3();
+            Console.Read();*/
+
+            Console.WriteLine("Main Thread Started");
+            //Creating Threads
+            Thread t1 = new Thread(Method1)
+            {
+                Name = "Thread1"
+            };
+            Thread t2 = new Thread(Method2)
+            {
+                Name = "Thread2"
+            };
+            Thread t3 = new Thread(Method3)
+            {
+                Name = "Thread3"
+            };
+            //Executing the methods
+            t1.Start();
+            t2.Start();
+            t3.Start();
+
+            Console.WriteLine("Main Thread Ended");
+            Console.Read();
+        }
+        static void Method1()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("Method1 :" + i);
+            }
+        }
+
+        static void Method2()
+        {
+            Console.WriteLine("Method2 Started using " + Thread.CurrentThread.Name);
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("Method2 :" + i);
+                if (i == 3)
+                {
+                    Console.WriteLine("Performing the Database Operation Started");
+                    //Sleep for 10 seconds
+                  //  Thread.Sleep(10000);
+                    Console.WriteLine("Performing the Database Operation Completed");
+                }
+            }
+            Console.WriteLine("Method2 Ended using " + Thread.CurrentThread.Name);
+        }
+        static void Method3()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("Method3 :" + i);
+            }
+        }
+    }
+}
