3edfdbac52153345894e521cf33fdb18505a0967
Md Rokonuzzaman Reza <44805545+EmonRezaBD@users.noreply.github.com>
2025-02-07 12:49:30 +0600

    Task, Lock, Mutex, and ChildThread
    Basic of all concepts

--- Multithreading_CSharp/MultiThreadProj/MultiThreadProj/Program.cs
+++ Multithreading_CSharp/MultiThreadProj/MultiThreadProj/Program.cs
@@ -112,68 +112,129 @@ namespace MultiThreadingProj
         {
             Console.WriteLine(message);
         }
-        static void Main(string[] args)
+
+        static void ChildTask()
         {
-            Program p1 = new Program();
+            Console.WriteLine("Child Task Started");
 
-            //p1.Add is args delegate function
-            //CThread AdditionThread1 = new CThread("Addtion", p1.Add); //if there is parameter then ThreadStart will not accept and we need lamda expression
-            CThread AdditionThread2 = new CThread("Addition", () => p1.Add(10, 20)); //Lamda expression
-            CThread SubsThread1 = new CThread("Subtraction", () => p1.Sub(10, 20));
-            CThread MulThread1 = new CThread("Multiplication", () => p1.Mul(10, 20, 2));
-            CThread DivThread1 = new CThread("Division", () => p1.Div());
-
-            //Task class
-
-            CTask taskObj = new CTask();
-            Task taskThread = new Task(() =>
+            //Child create grand child
+            Thread grandChildThread = new Thread(() =>
             {
-              taskObj.addTask();
-            }
-             );
-            taskThread.Start();
-
-            Console.WriteLine("Starting task...");
-
-            Task<int> task = Task.Run(() =>
-            {
-                // Simulating a long-running task
-                Task.Delay(2000).Wait();
-                return 42; // The answer to everything!
+                Console.WriteLine("Grandchild Thread started");
+                Thread.Sleep(1000);
+                Console.WriteLine("Grandchild Thread finished");
             });
 
-            //Delegate
-            DisplayMessage msgDelegate = testDelegate;
-            msgDelegate("This is from Delegate"); //invoking delegate method
+            grandChildThread.Start();
 
-            //Child Thread, Join operations
-            Thread chTh01 = new Thread(()=>childThread01());
-            Thread chTh02 = new Thread(() => childThread02());
+            grandChildThread.Join();  // Child waits for grandchild
+            Console.WriteLine("Child Thread finished.");
+        }
+        static void Main(string[] args)
+        {
+            /*  Program p1 = new Program();
 
-           // chTh01.Sleep(1000);
-            chTh01.Name = "Child Thread 01";
-            chTh02.Name = "Child Thread 02";
+              //p1.Add is args delegate function
+              //CThread AdditionThread1 = new CThread("Addtion", p1.Add); //if there is parameter then ThreadStart will not accept and we need lamda expression
+              CThread AdditionThread2 = new CThread("Addition", () => p1.Add(10, 20)); //Lamda expression
+              CThread SubsThread1 = new CThread("Subtraction", () => p1.Sub(10, 20));
+              CThread MulThread1 = new CThread("Multiplication", () => p1.Mul(10, 20, 2));
+              CThread DivThread1 = new CThread("Division", () => p1.Div());
 
-            chTh02.Start();
+              //Task class
 
-            chTh01.Start();
-            chTh01.Join();           
-            chTh02.Join();
+              CTask taskObj = new CTask();
+              Task taskThread = new Task(() =>
+              {
+                taskObj.addTask();
+              }
+               );
+              taskThread.Start();
 
-            Console.WriteLine("1. MainThread Started");
-            for (int i = 0; i <= 3; i++)
+              Console.WriteLine("Starting task...");
+
+              Task<int> task = Task.Run(() =>
+              {
+                  // Simulating a long-running task
+                  Task.Delay(2000).Wait();
+                  return 42; // The answer to everything!
+              });
+
+              //Delegate
+              DisplayMessage msgDelegate = testDelegate;
+              msgDelegate("This is from Delegate"); //invoking delegate method
+
+              //Child Thread, Join operations
+              Thread chTh01 = new Thread(()=>childThread01());
+              Thread chTh02 = new Thread(() => childThread02());
+
+             // chTh01.Sleep(1000);
+              chTh01.Name = "Child Thread 01";
+              chTh02.Name = "Child Thread 02";
+
+              chTh02.Start();
+
+              chTh01.Start();
+              chTh01.Join();           
+              chTh02.Join();
+
+              Console.WriteLine("1. MainThread Started");
+              for (int i = 0; i <= 3; i++)
+              {
+                  Console.WriteLine("-> MainThread Executing");
+                  //Thread.Sleep(2000); // Here 5000 is 5000 Milli Seconds means 5 Seconds
+              }
+
+              //Test Mutex
+
+              Example mutObj = new Example();
+              mutObj.threadCreation();*/
+
+
+            //Parent and Child Task
+            //parent creating a child task
+            /* Console.WriteLine("Parent Thread started.");
+             Thread childThread = new Thread(new ThreadStart(ChildTask));
+             childThread.Start();
+
+
+             Console.WriteLine("Parent Thread continues working...");
+             childThread.Join();
+             Console.WriteLine("Parent Thread ending.");*/
+
+            //Using Task Class
+            Console.WriteLine("Parent Task started.");
+
+            // Create a parent task
+            Task parentTask = Task.Factory.StartNew(() =>
             {
-                Console.WriteLine("-> MainThread Executing");
-                //Thread.Sleep(2000); // Here 5000 is 5000 Milli Seconds means 5 Seconds
-            }
+                Console.WriteLine("Parent Task is running...");
 
-            //Test Mutex
+                // Create a child task attached to the parent
+                Task childTask = Task.Factory.StartNew(() =>
+                {
+                    Console.WriteLine("Child Task started.");
 
-            Example mutObj = new Example();
-            mutObj.threadCreation();
+                    // Create a grandchild task attached to the child
+                    Task grandChildTask = Task.Factory.StartNew(() =>
+                    {
+                        Console.WriteLine("Grandchild Task started.");
+                        Task.Delay(1000).Wait();  // Simulate work
+                        Console.WriteLine("Grandchild Task finished.");
+                    }, TaskCreationOptions.AttachedToParent);
 
-            //Console.ReadKey(); // Prevents the program from exiting immediately
+                    grandChildTask.Wait(); // Ensure grandchild finishes before moving on
+                    Console.WriteLine("Child Task finished.");
 
+                }, TaskCreationOptions.AttachedToParent);
+            });
+
+            // Wait for parent, child, and grandchild tasks to complete
+            parentTask.Wait();
+
+            Console.WriteLine("Parent, Child, and Grandchild Tasks completed.");
+
+            Console.ReadKey(); // Prevents the program from exiting immediately
         }
     }
 }
--- Multithreading_CSharp/MultiThreadProj/MultiThreadProj/obj/Debug/net8.0/MultiThreadProj.AssemblyInfo.cs
+++ Multithreading_CSharp/MultiThreadProj/MultiThreadProj/obj/Debug/net8.0/MultiThreadProj.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MultiThreadProj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46fa75eeee714f5cb7e8716415d38a427f600d34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e790d8eb41143dd4979ee341a7d9ed5213d88467")]
 [assembly: System.Reflection.AssemblyProductAttribute("MultiThreadProj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MultiThreadProj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
