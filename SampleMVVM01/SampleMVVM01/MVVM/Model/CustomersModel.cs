7953a1e7215001c647dd52aa3994c5a659dc0e3a
Md Rokonuzzaman Reza <44805545+EmonRezaBD@users.noreply.github.com>
2025-02-14 16:55:27 +0600

    MVVM 01

---
+++ SampleMVVM01/SampleMVVM01/MVVM/Model/CustomersModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleMVVM01.MVVM.Model
+{
+    public class CustomersModel
+    {
+        //Fields or attributes
+        private string full_name;
+
+        //Contructors
+        CustomersModel(string full_name)
+        {
+            this.full_name = full_name;
+        }
+
+        //Property
+        public string Full_Name
+        {
+            get { return full_name; } //getter
+            set { full_name = value; }//setter
+        }
+
+        //Method
+
+    }
+}
---
+++ SampleMVVM01/SampleMVVM01/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SampleMVVM01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //MVVM
+            //step-01: first design the View : Design of App, no C# code, only XAML code
+            //step-02: second design the Model : Connector using Binding & Commands
+            //step-03: third design the ViewModel : Data, C# code, File, etc, Business login
+
+
+        }
+    }
+}
