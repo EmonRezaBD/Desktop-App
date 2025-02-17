2217cd0a40a53ebff8166cfd70b71df678fbe974
Md Rokonuzzaman Reza <44805545+EmonRezaBD@users.noreply.github.com>
2025-02-17 16:18:22 +0600

    TCP IP C#

---
+++ Socket Programming/Server/Client/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Program
+    {
+        static Socket sck;
+        static void Main(string[] args)
+        {
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+
+            try
+            {
+                sck.Connect(localEndPoint);
+            }
+            catch
+            {
+                Console.Write("Unable to connect remote end Point!!\r\n");
+                Main(args);
+            }
+
+            Console.Write("Enter Text: ");
+            string text = Console.ReadLine();
+            byte[] data = Encoding.ASCII.GetBytes(text);
+
+            sck.Send(data);
+            Console.WriteLine("Data Sent!!");
+            Console.WriteLine("Press any key to continue...");
+            Console.Read();
+
+            sck.Close();
+        }
+    }
+}
