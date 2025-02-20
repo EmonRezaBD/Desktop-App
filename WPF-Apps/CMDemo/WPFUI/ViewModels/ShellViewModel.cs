9d545bf6c74bb9d1b5d7fdfb98d91bde0581d27e
Md Rokonuzzaman Reza <44805545+EmonRezaBD@users.noreply.github.com>
2025-02-20 16:15:35 +0600

    NotifyProperty

--- WPF-Apps/CMDemo/WPFUI/ViewModels/ShellViewModel.cs
+++ WPF-Apps/CMDemo/WPFUI/ViewModels/ShellViewModel.cs
@@ -7,21 +7,45 @@ using Caliburn.Micro;
 
 namespace WPFUI.ViewModels
 {
-    public class ShellViewModel:Screen
-    {
-		private string _firstName="Reza";
+	public class ShellViewModel : Screen
+	{
+		private string _firstName = "Reza";
 
 		public string FirstName
-        {
-			get 
-			{ 
-				return _firstName; 
-			}
-			set 
+		{
+			get
 			{
-				_firstName = value; 
+				return _firstName;
+			}
+			set
+			{
+				_firstName = value;
+				NotifyOfPropertyChange(() => FirstName);
+				NotifyOfPropertyChange(()=>FullName);
 			}
 		}
+
+		private string _lastName;
+
+		public string LastName
+		{
+			get { return _lastName; }
+			set
+			{
+				_lastName = value;
+				NotifyOfPropertyChange(() => LastName);
+                NotifyOfPropertyChange(() => FullName);//when first or last name changes, the full name change
+
+            }
+        }
+		//propfull tab tba
+
+
+		public string FullName
+		{
+			get { return $"{FirstName} {LastName}"; }
+		}
+
 
 	}
 }
